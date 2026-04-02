dfe8483ea9386818bc5e78540b45a18a5ff79bb2
eniyavanthangabharathi-a11y <eniyavan.thangabharathi@gmail.com>
2026-04-02 12:07:21 +0530

    day3:zero counter adn control_output topic

--- install/pendulum_sim/lib/python3.10/site-packages/pendulum_sim/controller_node.py
+++ install/pendulum_sim/lib/python3.10/site-packages/pendulum_sim/controller_node.py
@@ -1,29 +1,45 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float64
+from std_msgs.msg import Float64, String
 
 class ControllerNode(Node):
 
-	def __init__(self):
-		super().__init__('controller')
-		self.subscription = self.create_subscription(
-					Float64,'pendulum_angle',
-					self.angle_callback,
-					10)
-	def angle_callback(self, msg):
-		angle = msg.data
-		if angle >0.1:
-			self.get_logger().info(f'Angle: {angle:.3f} - Leaning RIGHT, push left')
-		
-		elif angle <0.1:
-			self.get_logger().info(f'Angle: {angle:.3f} - Leaning LEFT, push right')
-		
-		else:
-			self.get_logger().info(f'Angle: {angle:.3f} - BALANCED')
+    def __init__(self):
+        super().__init__('controller')
+        self.subscription = self.create_subscription(
+            Float64,
+            'pendulum_angle',
+            self.angle_callback,
+            10)
+        self.publisher_ = self.create_publisher(String, 'control_output', 10)
+        self.prev_angle = 0.0
+        self.zero_crossings = 0
 
-def main(args =None):
-	rclpy.init(args=args)
-	node = ControllerNode()
-	rclpy.spin(node)
+    def angle_callback(self, msg):
+        angle = msg.data
+        command = String()
+
+        if self.prev_angle * angle < 0:
+            self.zero_crossings += 1
+            self.get_logger().info(f'Zero crossing #{self.zero_crossings}')
+
+        if angle > 0.1:
+            command.data ='push_left'
+            self.get_logger().info(f'Angle: {angle:.3f} — push_left')
+        elif angle < -0.1:
+            command.data = 'push_right'
+            self.get_logger().info(f'Angle: {angle:.3f} — push_right')
+        else:
+            command.data = 'hold'
+            self.get_logger().info(f'Angle: {angle:.3f} — BALANCED, hold')
+
+        self.publisher_.publish(command)
+        self.prev_angle = angle
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = ControllerNode()
+    rclpy.spin(node)
+
 if __name__ == '__main__':
-	main()
+    main()
